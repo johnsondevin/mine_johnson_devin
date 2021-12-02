84bacdbd0d0b8b277940f1ac7d9e6923a1de62cf
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-12-02 16:01:02 -0700

    edited main.py

--- __main__.py
+++ __main__.py
@@ -1,14 +1,27 @@
 import random
 from game.director import Director
+from game.actor import Actor
+from game.squares import Squares
+from game.draw_squares import DrawSquare
+from game.location import Location
+from game.input_service import InputService
+from game.output_service import OutputService
+from game.clear_space import ClearSpace
 from asciimatics.screen import Screen
 
-def main():
+def main(screen):
     cast = {}
     script = {}
 
-    script['input'] = 
-    script['update'] = 
-    script['output'] = 
+    input_service = InputService(screen)
+    output_service = OutputService(screen)
+    draw_squares = DrawSquare()
+    clear_space = ClearSpace(output_service)
+    
+
+    script['input'] = []
+    script['update'] = []
+    script['output'] = []
 
     director = Director(cast, script)
     director.start_game()
