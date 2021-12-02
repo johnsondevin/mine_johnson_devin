7d7e3f020cf7c10c6f43efccf61e93d58f152ee3
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-12-02 16:11:21 -0700

    More setup

--- __main__.py
+++ __main__.py
@@ -7,6 +7,8 @@ from game.location import Location
 from game.input_service import InputService
 from game.output_service import OutputService
 from game.clear_squares import ClearSquares
+from game.choose_square import ChooseSquare
+from game.handle_selection_squares import HandleSelectionSquare
 from asciimatics.screen import Screen
 
 def main(screen):
@@ -15,13 +17,15 @@ def main(screen):
 
     input_service = InputService(screen)
     output_service = OutputService(screen)
-    draw_squares = DrawSquares()
-    clear_squares = ClearSquares(output_service)
+    draw_squares = DrawSquares(output_service)
+    clear_squares = ClearSquares()
+    choose_square = ChooseSquare
+    handle_selection_square = HandleSelectionSquare()
     
 
-    script['input'] = []
-    script['update'] = []
-    script['output'] = []
+    script['input'] = [choose_square]
+    script['update'] = [clear_squares, handle_selection_square]
+    script['output'] = [draw_squares]
 
     director = Director(cast, script)
     director.start_game()
--- game/clear_squares.py
+++ game/clear_squares.py
@@ -1,2 +1,3 @@
 class Remove:
-    pass
+    def __init__(self, input_service):
+        self._input_service = input_service
